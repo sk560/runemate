07a7b0f7c3991fd9feaa81969e3053dcf5074ce5
Erik de Kamps <erikdekamps@gmail.com>
2015-02-10 22:46:45 +0100

    Added Stop() when out of materials.

--- com/runemate/geashawscripts/LazyAutoTanner/Tasks/Exit.java
+++ com/runemate/geashawscripts/LazyAutoTanner/Tasks/Exit.java
@@ -1,6 +1,10 @@
 package com.runemate.geashawscripts.LazyAutoTanner.Tasks;
 
+import com.runemate.game.api.hybrid.Environment;
+import com.runemate.game.api.hybrid.local.hud.interfaces.Bank;
 import com.runemate.game.api.script.framework.task.Task;
+import com.runemate.geashawscripts.LazyAutoTanner.Constants;
+import com.runemate.geashawscripts.LazyAutoTanner.Methods;
 
 /**
  * Created by Geashaw on 6-2-2015.
@@ -9,11 +13,12 @@ public class Exit extends Task {
 
     @Override
     public boolean validate() {
-        return false;
+        return Bank.isOpen() && Bank.getQuantity(Constants.hide) < 23;
     }
 
     @Override
     public void execute() {
-
+        Methods.debug("Ran out of material, stopping script.");
+        Environment.getScript().stop();
     }
 }
